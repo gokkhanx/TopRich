5634a85f73d925e2e69389e6d4ae0a9ad86970c2
Gökhan <67488323+gokkhanx@users.noreply.github.com>
2020-06-28 15:07:52 +0300

    Update Base.php
    String value does not return, Player undefined in BaskTask

--- src/TopRich/Base.php
+++ src/TopRich/Base.php
@@ -4,31 +4,34 @@ namespace TopRich;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\{Player, Server};
-use pocketmine\command\{CommandSender as Cs, Command as Cmd};
 use onebone\economyapi\EconomyAPI;
-use pocketmine\entity\Entity;
-use pocketmine\scheduler\TaskScheduler;
 
 class Base extends PluginBase implements Listener{
-	
+
 
 
 	  public static $default = ["Null" => 1];
-	
+
 	  public function onEnable(){
 	  	$this->getLogger()->info("Aktif.");
 	  	$this->economy = EconomyAPI::getInstance();
 	  	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		  $this->getScheduler()->scheduleDelayedTask(new BaseTask($this, $player), 1);
 	  }
-
-   
-	  
-	
-	  public function getTopTenList() : string{
+		public function onJoin(PlayerJoinEvent $event){
+			$x = 256;
+			$y = 56;
+			$z = 256;
+			$particle = new FloatingTextParticle(new Vector3($x, $y, $z), $this->getTopTenList());
+			$this->getServer()->getDefaultLevel()->addParticle($particle, [$event->getPlayer()]);
+		}
+	  public function getTopTenList(){
         $data = $this->economy->getAllMoney() ?? self::$default;
         arsort($data);
         $i = 1;
@@ -37,10 +40,10 @@ class Base extends PluginBase implements Listener{
             $text .= TextFormat::DARK_GRAY . "[" . TextFormat::AQUA . $i . TextFormat::DARK_GRAY . "] " . TextFormat::YELLOW . $key . TextFormat::DARK_RED . " => " . TextFormat::GOLD . $value . TextFormat::DARK_PURPLE . " Para" . TextFormat::EOL;
             $i++;
         }
-        return $text;
+				return $text;
     }
 
-    public function getTitle() : string{
+    public function getTitle(){
         return TextFormat::GRAY . "-==[ " . TextFormat::GREEN . "Sunucudaki en zengin 10 kişi" . TextFormat::GRAY . " ]==-";
     }
 }
